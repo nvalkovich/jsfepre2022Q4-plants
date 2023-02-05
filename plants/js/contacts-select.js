619345b065cc5a1537fa46dfb23fb19301228534
Anastasiya Valkovich <anastasiya.valkovich@mail.ru>
2023-02-06 02:24:16 +0300

    fix: fix contact select and css

--- plants/js/contacts-select.js
+++ plants/js/contacts-select.js
@@ -2,10 +2,16 @@ document.addEventListener('DOMContentLoaded', function () {
   const contactsSelect = document.querySelector('.contacts-select');
   const contentLinks = document.querySelectorAll('.select-content-link');
   const adressCards = document.querySelectorAll('.contacts-adress');
+  const contactImg = document.querySelector('.contact-img');
 
-  contactsSelect.addEventListener('click', () => {
+  contactsSelect.addEventListener('click', (e) => {
+    e.stopPropagation();
     contactsSelect.classList.toggle('contacts-select-active');
   });
+
+  window.onclick = (e) => {
+    contactsSelect.classList.remove('contacts-select-active');
+  };
 
   contentLinks.forEach((link) => {
     link.addEventListener('click', () => {
@@ -18,7 +24,9 @@ document.addEventListener('DOMContentLoaded', function () {
         if (card.dataset.name === link.dataset.name) {
           card.classList.add('contacts-adress-active');
         }
-      })
+      });
+
+      contactImg.classList.add('contact-img-hidden');
 
       contactsSelect.classList.remove('contacts-select-active');
     })
